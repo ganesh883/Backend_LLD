b36d8bcf36dd6294d045ad2b9ae73759e63cb782
Ganesh K <ganesh150eee@gmail.com>
2025-10-10 16:31:07 +0530

    Lambdas and Streams

---
+++ src/main/java/org/example/FunctionalInterface/TestInterface.java
@@ -0,0 +1,12 @@
+package org.example.FunctionalInterface;
+
+@FunctionalInterface
+public interface TestInterface<T> {
+    void fun1(T t);
+
+    default void fun2() {
+        System.out.println("Default Method");
+    }
+
+//    void fun2();
+}
