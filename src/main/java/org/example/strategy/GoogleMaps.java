b1748e611788fbc52c8807039827fd1752f06e20
Ganesh K <ganesh150eee@gmail.com>
2025-10-25 00:04:51 +0530

    Updated Code for tictactoe

--- src/main/java/org/example/strategy/GoogleMaps.java
+++ src/main/java/org/example/strategy/GoogleMaps.java
@@ -3,9 +3,9 @@ package org.example.strategy;
 
 public class GoogleMaps {
     public void findPath(String from, String to, String mode) {
-        strategy.PathCalculatorStrategy pathCalculatorStrategy =
-                PathCalculatorFactory.getPathCalculatorForMode(mode);
-
-        pathCalculatorStrategy.findPath(from, to);
+//        strategy.PathCalculatorStrategy pathCalculatorStrategy =
+//                PathCalculatorFactory.getPathCalculatorForMode(mode);
+//
+//        pathCalculatorStrategy.findPath(from, to);
     }
 }
